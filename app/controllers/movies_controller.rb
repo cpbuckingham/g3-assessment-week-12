54c864c87ca2024b23a3e6146c3613db1d299993
Cameron <cameron.p.buckingham@gmail.com>
2014-08-25 11:04:51 -0600

    all set

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,11 +12,11 @@ class MoviesController < ApplicationController
     @movie = Movie.new(name: params[:movie][:name],
                        year: params[:movie][:year],
                        synopsis: params[:movie][:synopsis])
+
+
     if @movie.save
-      flash[:notice] = "Movie added successfully!"
       redirect_to root_path
     else
-      @movie.errors
       render :new
     end
   end
